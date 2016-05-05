dbf9f2ffec4a2613eaba64c2e2b09530aa6e47a1
drsung <drsung2410@gmail.com>
2016-05-05 11:17:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import problem
 
-# Example solve problem
-
 newProblem = problem.Problem()
 solve = problem.Solve_Problem()
+newProblem.setStartState([[5,4,2],[7,8,3],[1,6,0]])
+newProblem.setGoalState([[1,2,3],[8,0,4],[7,6,5]])
 
 print("Goal State:")
 solve.printGoalState(newProblem)
