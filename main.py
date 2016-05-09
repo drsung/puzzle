0c1d7ec09b036103e951defe4c99dbdd2d21a6fd
drsung <drsung2410@gmail.com>
2016-05-09 08:52:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import problem
+import time
 
 newProblem = problem.Problem()
 solve = problem.Solve_Problem()
-newProblem.setStartState([[5,4,2],[7,8,3],[1,6,0]])
+
+matrix = []
+for i in range(3):
+	temp = []
+	for j in range(3):
+		temp.append(int(input()))
+	matrix.append(temp)
+
+#matrix = [[2,1,3],[8,0,4],[6,7,5]]
+
+
+newProblem.setStartState(matrix)
 newProblem.setGoalState([[1,2,3],[8,0,4],[7,6,5]])
 
 print("Goal State:")
@@ -12,5 +24,7 @@ print("Start State:")
 solve.printStartState(newProblem)
 print('\n')
 print("Answer:")
+start_time = time.time()
 solve.printAnswer(newProblem)
-solve.doAction(newProblem)
+#solve.doAction(newProblem)
+print("Time: %s seconds" %(time.time() - start_time))
